615349b7539a69f0af51f93654e597cbd35ec958
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-08 10:25:25 -0400

    Mover ejercicio 6 a la carpeta del practico

--- Practico2/ejercicio6_solucion.cpp
+++ Practico2/ejercicio6_solucion.cpp
@@ -3,7 +3,7 @@ SOLUCION Ejercicio 6
 Escriba un programa que lea el nombre y el apellido separados por un espacio y genere el
 nombre completo en el formato <Apellido> , <Nombre> . Utilice el operador de suma para
 concatenar (unir) partes de cadenas en una sola cadena antes de generarlas, o
-imprímalas individualmente en el orden deseado.
+imprÃ­malas individualmente en el orden deseado.
 */
 #include <iostream>
 using namespace std;
