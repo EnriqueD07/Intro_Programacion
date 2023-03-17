c4387cd839a42628b68e1a0d7c154e3a86184fbe
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-17 11:27:03 -0400

    Update ejercicio1_solucion.cpp

--- practico3/ejercicio1_solucion.cpp
+++ practico3/ejercicio1_solucion.cpp
@@ -6,8 +6,33 @@ la casa con la que necesita hablar sobre su mascota, Fluffy.
 
 #include <iostream>
 using namespace std;
-int main ()
+
+int main()
 {
-  
-  return 0;
+    string casa1,casa2,casa3,casa;
+    cin>>casa1>>casa2>>casa3>>casa;
+    if (casa1!=casa2!=casa3)
+    {
+    if (casa1==casa)
+    {
+        cout<<"Entra en la casa 1"<<endl;
+    }
+    else if (casa2==casa)
+    {
+        cout<<"Entra en la casa 2"<<endl;
+    }
+    else if (casa3==casa)
+    {
+        cout<<"Entra en la casa 3"<<endl;
+    }
+    else
+    {
+        cout<<"Busca en otro lugar"<<endl;
+    }
+    }
+    else{
+        cout<<"No pueden haber casas de colores iguales";
+    }
+    return 0;
 }
+
