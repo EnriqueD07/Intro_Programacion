21a8467c4dfc667f6c52181dc8cafe37ad3ec0b1
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-16 22:24:59 -0400

    Analisis de entradas y salidas

--- Practico2/ejercicio16_solucion.cpp
+++ Practico2/ejercicio16_solucion.cpp
@@ -33,22 +33,22 @@ print No cuenta con la edad necesaria para el puesto
 using namespace std;
 int main()
 {
-    int edad,aexperiencia; // declaramos las variables edad y aexperiencia aexperiencia son los años de experiencia 
-    cin>>edad>>aexperiencia;
-    if (edad>=18)
+    int edad,aexperiencia;  // declaramos las variables edad y aexperiencia aexperiencia son los años de experiencia 
+    cin>>edad>>aexperiencia;    //Introducimos las variables edad y experiencia
+    if (edad>=18) //Si la edad es mayor o igual a 18, se ejecuta el codigo
     {
-        if (aexperiencia>=5)
+        if (aexperiencia>=5)    //Si la experiencia es mayor o igual 5, se ejecuta el siguiente codigo
         {
-            cout<<"director senior";
-        } else if (aexperiencia>=3)
+            cout<<"director senior";     //Muestra en consola el texto entre ""
+        } else if (aexperiencia>=3) //Si no se cumple la condición anterior y la experiencia es mayor o igual a 3, se ejecuta el siguiente codigo
         {
-            cout<<"director de proyecto";
-        } else{
-            cout<<"coordinador de proyecto";
+            cout<<"director de proyecto";   //Muestra en consola el texto entre ""
+        } else{             //Si no se cumple la condicion anterior se ejectuta el siguiente codigo
+            cout<<"coordinador de proyecto";    //Muestra en consola el texto entre ""
         }
     }
-    else {
-        cout<<"No cuenta con la edad necesaria para el puesto";
+    else {      //si no se cumple nada de lo anterior, se ejecuta el siguiente codigo
+        cout<<"No cuenta con la edad necesaria para el puesto";     //Muestra en consola el texto entre ""
     }
     return 0;
 }
