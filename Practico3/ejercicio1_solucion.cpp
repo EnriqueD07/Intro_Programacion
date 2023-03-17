3fd053ab71a1bd7585a784b882bff6177fc6226b
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-17 11:14:05 -0400

    Ejercicio 1

---
+++ Practico3/ejercicio1_solucion.cpp
@@ -0,0 +1,13 @@
+/*
+Frente a Zoe hay tres casas de diferentes colores. Se sabe junto a qué color de casa se
+vio por última vez a Fluffy . Las casas están numeradas 1, 2 y 3. Determina el número de
+la casa con la que necesita hablar sobre su mascota, Fluffy.
+*/
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+  
+  return 0;
+}
