797d2b06774453855749126e966c6d97e31bd1dd
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-18 20:08:12 -0400

    Ejercicio corregido
    se habia subido el 10 en vez del 9 :p

--- Practico2/ejercicio9_solucion.cpp
+++ Practico2/ejercicio9_solucion.cpp
@@ -1,19 +1,21 @@
 /*
-SOLUCION EJERCICIO 10
-Escriba un programa que lea un número (número de documentos) y muestre <número de
-documentos> documentos encontrados . Primero, forme una cadena con la respuesta, solo
-luego muéstrela en la pantalla en su totalidad.
+SOLUCION EJERCICIO 9
+Escriba un programa que lea dos enteros no negativos separados por un espacio. El
+programa debe calcular su suma S y generar el producto de S por el número de dígitos en
+la notación decimal S.
 */
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
-    int num;
-    string doc;
-    cin>>num;
-    string numero=to_string(num);
-    doc=numero+" documentos encontrados."s;
-    cout<<doc;
+    int num1,num2,S,digitos;
+    float prod;
+    cin>>num1>>num2;
+    S=num1+num2; //sumamos los numeros
+    string dig = to_string(S);  // Convertimos la suma en una cadena
+    digitos=dig.size();  //Calculamos los caracteres de la cadena dig
+    prod=digitos*S; //multiplicamos el numero de caracteres por la suma
+    cout<<prod;
     return 0;
 }
