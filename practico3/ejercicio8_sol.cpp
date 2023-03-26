d8a8dc5aeee27a2c72d6f51987dc09168c73e602
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-26 19:57:36 -0400

    ejercicio 8 dudas

---
+++ practico3/ejercicio8_sol.cpp
@@ -0,0 +1,15 @@
+Ejercicio 8
+Ahora escriba las palabras encerradas entre corchetes. Por ejemplo, si la entrada fue un
+green parrot , la salida debería ser:
+Puedes reutilizar el algoritmo del problema anterior. Allí ibas a lo largo de la línea y
+verificaste si el caracter era un espacio. Si el carácter no era un espacio, simplemente lo
+saltabas y seguía adelante. Ahora este caso tendrá que ser procesado.
+Cree una variable de cadena, word , en la que agregará carácter por carácter de la
+cadena original, si este carácter no es un espacio. Si se encuentra un espacio, muestre
+[ , la palabra acumulada y ] y asigne una cadena vacía a la variable, word , para reiniciar
+el proceso con la siguiente palabra de la cadena.
+Restricciones
+Lo mismo que en el ejercicio anterior:
+Hay un espacio entre las palabras.
+Una línea comienza con una palabra y termina con el final de una línea.
+Hay exactamente una línea en la consulta.
