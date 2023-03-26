feea38cca331bd48c7b21806fa4e90223361b54d
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-26 19:55:26 -0400

    ejercicio 7 dudas

---
+++ practico3/ejercicio7_sol.cpp
@@ -0,0 +1,23 @@
+/*
+Para buscar documentos, debe poder dividir el texto en palabras. Inicialmente, lo
+haremos de una forma no estandar: escriba su propio algoritmo.
+Las palabras están separadas por espacios. Se garantiza exactamente un espacio entre
+palabras. La línea comienza con una palabra (no un espacio) y termina con el final de la
+línea ( \n ). Solo se ingresa una línea, el final de la línea también es uno, y alguna palabra
+siempre termina antes.
+Lea toda la línea desde la terminal hasta una nueva línea. Para cada palabra, imprima el
+índice después del último carácter de la palabra. Recuerde, la indexación comienza
+desde cero.
+Restricciones
+Hay un espacio entre las palabras. No hay espacios al principio o al final de la cadena.
+Hay exactamente una línea en la consulta.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+ string query;
+ getline(cin, query);
+// Su codigo aqui
+}
