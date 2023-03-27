0ab9a6c0caf7c33acf6c1af854f845d620754877
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-26 20:37:50 -0400

    ejercicio 9 dudas

---
+++ practico3/ejercicio9_sol.cpp
@@ -0,0 +1,12 @@
+Ejercicio 9
+Según una encuesta realizada por Most Accurate Statistics Bureau , los encuestados
+hacen un promedio de 10 visitas al refrigerador por noche. Solo una circunstancia puede
+detenerlos: Ya se acabo la torta de la heladera .
+Escriba un programa que simule las aventuras nocturnas del ciudadano. Puede averiguar
+si hay un pastel leyendo el número de cin . Solo tomará dos valores:
+1 - todavía hay torta,
+0 - se acabó la torta.
+Si se encuentra un pastel, imprima la cadena Om-nom-nom :P . Si se acabó el pastel,
+escriba Sin pastel :( y finalice el ciclo con una declaración de break . Tambien, debe
+ignorarse ingresar más de 10 unidades, ya que el ciudadano promedio ya se ha quedado
+sin pastel en este caso.
