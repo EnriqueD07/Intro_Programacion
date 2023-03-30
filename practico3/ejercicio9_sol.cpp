21937204bdfaf65fcb1b5208154f532bfdeecb3b
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-30 15:52:49 -0400

    Update ejercicio9_sol.cpp

--- practico3/ejercicio9_sol.cpp
+++ practico3/ejercicio9_sol.cpp
@@ -1,3 +1,4 @@
+/*
 Ejercicio 9
 Según una encuesta realizada por Most Accurate Statistics Bureau , los encuestados
 hacen un promedio de 10 visitas al refrigerador por noche. Solo una circunstancia puede
@@ -10,3 +11,22 @@ Si se encuentra un pastel, imprima la cadena Om-nom-nom :P . Si se acabó el pas
 escriba Sin pastel :( y finalice el ciclo con una declaración de break . Tambien, debe
 ignorarse ingresar más de 10 unidades, ya que el ciudadano promedio ya se ha quedado
 sin pastel en este caso.
+*/
+#include <iostream>
+using namespace std;
+int main() {
+ // Escriba un ciclo for, que realice 10 idas a la heladera
+    bool nom;
+    for (int i ; i<=10 ; ++i)
+    {
+        cin>>nom;
+        if (nom==1)
+        {
+        a++
+        }
+        if (nom==0)
+        {
+        break;
+        }
+    }
+}
