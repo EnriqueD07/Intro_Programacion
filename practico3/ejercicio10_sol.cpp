797bb0d145cc1847cf5db1fb83bbc9b481e48112
Enrique Diaz <124794674+EnriqueD07@users.noreply.github.com>
2023-03-31 22:29:14 -0400

    Update ejercicio10_sol.cpp

--- practico3/ejercicio10_sol.cpp
+++ practico3/ejercicio10_sol.cpp
@@ -1,16 +1,3 @@
-/*
-El operador de continue se usa a menudo justo al comienzo de un ciclo para eliminar
-casos innecesarios. Le permite simplificar el código y eliminar el largo if .
-En el código - un programa para calcular el beneficio neto de la empresa. Este lee
-ingresos y gastos de cin , pero suma solo cantidades positivas: ingresos. Todas las
-cantidades negativas se ignoran.
-El programa funciona muy bien, pero resultó que la persona que revisó el código no
-conoce la declaración de continue , por lo que el cliente exige que se elimine.
-¡Qué se puede hacer, la palabra del cliente es la ley! Empeora el código de tu programa
-sin cambiar su comportamiento deshaciéndote de la instrucción continue .
-Es decir, el programa debe realizar lo mismo pero ahora sin utilizar la palabra clave
-continue .
-*/
 #include <iostream>
 using namespace std;
 int main() {
@@ -30,3 +17,18 @@ int main() {
  } while (bill != 0);
  cout << "Total income: " << sum << endl;
 }
+
+
+/* EJERCICIO 10
+El operador de continue se usa a menudo justo al comienzo de un ciclo para eliminar
+casos innecesarios. Le permite simplificar el código y eliminar el largo if .
+En el código - un programa para calcular el beneficio neto de la empresa. Este lee
+ingresos y gastos de cin , pero suma solo cantidades positivas: ingresos. Todas las
+cantidades negativas se ignoran.
+El programa funciona muy bien, pero resultó que la persona que revisó el código no
+conoce la declaración de continue , por lo que el cliente exige que se elimine.
+¡Qué se puede hacer, la palabra del cliente es la ley! Empeora el código de tu programa
+sin cambiar su comportamiento deshaciéndote de la instrucción continue .
+Es decir, el programa debe realizar lo mismo pero ahora sin utilizar la palabra clave
+continue .
+*/
